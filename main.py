2e5a44db153e1732c3585ccc6e6f7fba7ba8a79d
Oliver Rupprecht <oliverjrupprecht@gmail.com>
2025-08-26 14:43:11 +0100

    refactored to accept args from cli

--- main.py
+++ main.py
@@ -3,19 +3,26 @@ from stats import get_num_characters
 from stats import make_new_dict
 from stats import sort_by_apperance
 
+import sys
+
+
 def main():
-    filepath = "books/frankenstein.txt"
-    cleaned = sort_by_apperance(get_num_characters(filepath))
-    wordcount = get_num_words(filepath)
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        filepath = sys.argv[1]
+        cleaned = sort_by_apperance(get_num_characters(filepath))
+        wordcount = get_num_words(filepath)
 
-    print(f"============ BOOKBOT ============\nAnlyzing book found at {filepath}")
-    print(f"----------- Word Count ----------\nFound {wordcount} total words")
-    print(f"--------- Character Count -------")
+        print(f"============ BOOKBOT ============\nAnlyzing book found at {filepath}")
+        print(f"----------- Word Count ----------\nFound {wordcount} total words")
+        print(f"--------- Character Count -------")
     
-    for dict in cleaned:
-        print(f"{dict["char"]}: {dict["num"]}")
+        for dict in cleaned:
+            print(f"{dict["char"]}: {dict["num"]}")
 
-    print("============= END ===============")
+        print("============= END ===============")
 
 main()
 
